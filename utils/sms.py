ac20d28d7595a1d260e731286b0a2c1f9366dfeb
Amir Voria Khosravi <amirpba@gmail.com>
2025-05-20 12:15:02 +0330

    Add sms ir api key

--- utils/sms.py
+++ utils/sms.py
@@ -1,12 +1,12 @@
 from sms_ir import SmsIr
-sms_ir = SmsIr("API")
+sms_ir = SmsIr("7C59DyoSD7b9dnrMlxYPfo3CoKoTragY60luWMxEtScjEMZP")
 
 
 
 def send_otp(phone,code):
     sms_ir.send_verify_code(
                 number=phone,
-                template_id=int("4324"),
+                template_id=int("830439"),
                 parameters=[
                         {
                             "name" : "CODE",
